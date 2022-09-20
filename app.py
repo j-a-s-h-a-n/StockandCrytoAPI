cd8c8da3a1c87ca5c2b93d7483b787a62528d379
j-a-s-h-a-n <73367977+j-a-s-h-a-n@users.noreply.github.com>
2022-09-19 19:41:54 -0700

    privacy changes

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ from flask_jwt_extended import JWTManager
 from db import db
 
 app = Flask(__name__)
-url='postgresql://pkajdrxdqpfskl:f7320a226552faf3518021767264eaccf5f15f47adc4d6ff4dd2ec23c6487ce9@ec2-52-207-90-231.compute-1.amazonaws.com:5432/d5jc9a3cj36kdi'
+url = '' #enter database URL
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 app.config['PROPAGATE_EXCEPTIONS']=True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-app.secret_key='jdkskhcsoemkfncncwienio'
+app.secret_key=''#enter secret key
 
 api = Api(app)
 jwt = JWTManager(app)
